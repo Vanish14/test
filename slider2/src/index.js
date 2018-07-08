273c57699eb01940dbb8551ca120ea0954a2ba10
Ivan <sportovah@mail.ru>
2018-07-08 14:10:40 +0300

    Комит

---
+++ slider2/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+function Hello(){
+	let phrase = 'World!'
+	return(
+		
+	<h1>Hello {phrase}!</h1>
+	)
+}
+
+ReactDOM.render(<Hello />, document.getElementById('root'));
+registerServiceWorker();
+ 
